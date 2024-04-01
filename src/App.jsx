bf5433a4bfcc5df2934485b597741194ac6eabfc
ViNu-23 <vijayvinu46@gmail.com>
2024-04-01 08:42:56 +0530

    set

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 export default function App() {
   return (
-    <div>App1</div>
+    <div>App</div>
   )
 }
---
+++ src/Pages/Popula.jsx
@@ -0,0 +1,5 @@
+export default function Popula() {
+  return (
+    <div>Popula</div>
+  )
+}
